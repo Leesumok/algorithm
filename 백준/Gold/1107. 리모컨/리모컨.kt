88363f10d2f74b8d9567e42c7d3b68fcf8860516
Sumok <leesumok.7@gmail.com>
2025-09-17 20:14:51 +0900

    [Gold IV] Title: 리모컨, Time: 1120 ms, Memory: 301872 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.kt
+++ 백준/Gold/1107. 리모컨/리모컨.kt
@@ -5,39 +5,23 @@ import kotlin.math.abs
 fun main() {
     val targetCh = readln().toInt()
     val brokenSize = readln().toInt()
-    val nowCh = 100
     val brokenBtnStateArr = BooleanArray(10)
-    var result = 0
 
-    if (targetCh != 100) { // 목표 채널이 100이 아닐때
-        val pushPlusMinus = abs(targetCh - nowCh)
 
-        if (brokenSize > 0) { // 고장난 버튼이 있을때
-            val brokenButton = readln().split(" ").map { it.toInt() }
-            for (button in brokenButton) {
-                brokenBtnStateArr[button] = true
-            }
+    if (brokenSize > 0) { // 고장난 버튼이 있을때
+        val brokenButton = readln().split(" ").map { it.toInt() }
+        for (button in brokenButton) {
+            brokenBtnStateArr[button] = true
         }
+    }
+    var result = abs(targetCh - 100)
+    for (ch in 0..999999) {
+        if (canUseChannel(brokenBtnStateArr, ch)) {
+            val digitCount = ch.toString().length
+            val moveCount = abs(targetCh - ch)
+            val totalCount = digitCount + moveCount
 
-        if (brokenSize > 0 && !canUseChannel(brokenBtnStateArr, targetCh)) {
-            var minCount = pushPlusMinus
-            for (ch in 0..999999) {
-                if(canUseChannel(brokenBtnStateArr, ch)){
-                    val digitCount = ch.toString().length
-                    val moveCount = abs(targetCh - ch)
-                    val totalCount = digitCount + moveCount
-
-                    if(totalCount < minCount){
-                        minCount = totalCount
-                    }
-                }
-            }
-
-            result = minCount
-
-        } else { // 고장난 버튼이 없거나 모든 버튼을 사용가능할때
-            val pushCh = targetCh.toString().length
-            result = if (pushPlusMinus > pushCh) pushCh else pushPlusMinus
+            result = minOf(result, totalCount)
         }
     }
 
