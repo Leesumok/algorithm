3c1ed8d8a4ebaaf4e6864757ebcede6b93c78c45
Sumok <leesumok.7@gmail.com>
2026-02-24 21:56:13 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 84 ms, Memory: 13540 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.kt
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.kt
@@ -1,20 +1,23 @@
 fun main() {
     var answer = 0
     repeat(readln().toInt()){
-        answer++
         val word = readln()
         val visited = mutableSetOf<Char>()
-        visited.add(word[0])
-        for (c in 1 until word.length){
-            if(!visited.contains(word[c])){visited.add(word[c])}
-            // 본적있는데 이전문자와도 다르다면
-            if(visited.contains(word[c]) && word[c-1] != word[c]){
-                val cutWord = word.substring(0, c)
-                if(cutWord.contains(word[c])){
-                    answer--
+        var prev: Char? = null
+        var isGroup = true
+        for (c in 0 until word.length){
+            if (prev != word[c]){
+                if(visited.contains(word[c])){
+                    isGroup = false
                     break
                 }
+                visited.add(word[c])
             }
+            prev = word[c]
+        }
+
+        if(isGroup){
+            answer++
         }
     }
     println(answer)
